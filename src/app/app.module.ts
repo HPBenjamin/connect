476028bd177dd3b242559212fe02bab9614caf5a
HPBenjamin <hptischoff@gmail.com>
2021-10-11 20:47:29 +0200

    added searching-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +40,9 @@ import { PostingsComponent } from './postings/postings.component';
     MatInputModule,
     TextFieldModule,
     MatIconModule,
-    ScrollingModule
+    ScrollingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
